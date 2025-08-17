da3d8a983439099402a5221803380b9465658e7a
David, Shin <dirmich@gmail.com>
2025-08-17 18:06:22 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
-import { Button } from "@/components/ui/button"
+// 'use client'
+// import { Button } from "@/components/ui/button"
+// import { useTRPC } from "@/trpc/client"
+// import { useQuery } from "@tanstack/react-query"
 
-const Page = () => {
+import { caller } from "@/trpc/server"
+
+// const Page = () => {
+//   const trpc = useTRPC()
+//   // trpc.createAI.queryOptions({text:"Hello"})
+//   // trpc.createAI.queryOptions({text:123})
+//   const { data } = useQuery(trpc.createAI.queryOptions({ text: "highmaru" }))
+//   console.log('->',data)
+//   return (
+//     <div>
+//       <Button variant='new'>
+//         Click me
+//       </Button>
+//     </div>
+//   )
+//  }
+
+
+const Page = async () => {
+  const data = await caller.createAI({ text: 'highmaru' })
+  console.log('->', data)
   return (
-    <div>
-      <Button variant='new'>
-        Click me
-      </Button>
-    </div>
+    <div>Hello</div>
   )
- }
+}
 
-export default Page
+export default Page 
